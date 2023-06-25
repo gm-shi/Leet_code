dfc8bdf688248df8719f8e28c2425212fc58a773
Gongming <gm.shi@outlook.com>
2023-06-25 11:09:38 +1000

    dp2

--- Coding.java
+++ Coding.java
@@ -1,11 +1,23 @@
 class Solution {
-    public int minCostClimbingStairs(int[] cost) {
-        int[] dp = new int[cost.length];
-        dp[0] = cost[0];
-        dp[1] = cost[1];
-        for (int i = 2; i < cost.length; i++) {
-            dp[i] = Math.min(dp[i - 1], dp[i - 2]) + cost[i];
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length, n = obstacleGrid[0].length;
+        if (obstacleGrid[m - 1][n - 1] == 1 || obstacleGrid[0][0] == 1) // 如果在起点或终点出现了障碍，直接返回0
+            return 0;
+        int dp[][] = new int[m][n];
+
+        for (int i = 0; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
         }
-        return Math.min(dp[cost.length - 1], dp[cost.length - 2]);
+        for (int i = 0; i < n && obstacleGrid[0][i] == 0; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+
+                dp[i][j] = obstacleGrid[i][j] == 1 ? 0 : dp[j - 1][i] + dp[j][i - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+
     }
 }
