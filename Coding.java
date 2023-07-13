91fc147476318c435749fc58b572803426cc77fd
Gongming <gm.shi@outlook.com>
2023-07-13 13:13:08 +1000

    dp

--- Coding.java
+++ Coding.java
@@ -1,5 +1,21 @@
 class Solution {
-    public int findMaxForm(String[] strs, int m, int n) {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 0;
 
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < coins.length; j++) {
+                if (i >= coins[j] && dp[i - coins[j]] != Integer.MAX_VALUE) {
+                    dp[i] = Math.min(dp[i - coins[j]] + 1, dp[i]);
+                }
+            }
+        }
+        if (dp[amount] == Integer.MAX_VALUE)
+            return -1;
+        return dp[amount];
     }
 }
